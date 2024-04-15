99b16f78c3fa974d85ec172caf0c708a1d2a899c
guerinorodella <guerinorodella@gmail.com>
2024-04-15 20:44:55 -0300

    feat(server): basic server implementation listening on default port.

---
+++ src/test/java/com/gear/dev/simplepdvapi/MainTests.java
@@ -0,0 +1,13 @@
+package com.gear.dev.simplepdvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
